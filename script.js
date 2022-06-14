4adaf68e9957f7604af1a355e93ad6be7e74643e
walter <arenalwm@gmail.com>
2022-06-14 00:58:59 -0300

    velocidad video animacion hover

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ menuToggle.addEventListener("click", () => {
     menuToggle.classList.toggle("active")
     showcase.classList.toggle("active")
 })
+document.querySelector("video").playbackRate = 0.5;
