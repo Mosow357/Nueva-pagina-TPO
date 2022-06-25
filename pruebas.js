3d34a6b454ec70a1e95e1bca10526c2b17017d3b
walter <arenalwm@gmail.com>
2022-06-25 14:17:41 -0300

    final

--- pruebas.js
+++
@@ -1,8 +0,0 @@
-<div class="carousel-item">
-              <img :src="imagen.webformatURL" class="d-block w-100" alt="...">
-              <div class="carousel-caption d-none d-md-block">
-          
-                <h5>{{imagen.tags}}</h5>
-          
-              </div>
-           </div>
